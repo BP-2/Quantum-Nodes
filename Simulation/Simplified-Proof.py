92bb34cc6a09114c6d27a57f35ccb181b807f6d4
Brady Phelps <bp309420@ohio.edu>
2023-07-24 10:29:54 -0400

    Simplified simulatoin

--- Simulation/Simplified-Proof.py
+++ Simulation/Simplified-Proof.py
@@ -2,6 +2,11 @@
 # Simulation of the simplified proof
 
 
+from qiskit import * 
+from qiskit.tools.visualization import plot_histogram
+from IPython.display import display
+
+
 # First we get the measurement that we will be performing on our values
 measure = input("Please type 'x' or 'z' for our measurement basis")
 
@@ -16,27 +21,39 @@ while x < 4:
 
 
 # Lets solve !!!
-if measure == "z" and (values[0] == "+" or values[0] == "-"):
-    print("Broken case")
-    
-    expanded_values = []
+if measure == "z" and (values[0] == "+" or values[0] == "-"):    
     # First we have to break '+' and '-' into |0> + |1> and |0> - |1> respectively
+    
+    # 5 bits (4 for storing data and 1 for storing XOR vals)
+    qc = QuantumCircuit(5,1)
+    counter = 0
     for val in values:
         if val == "+":
-            expanded_values.append("|0> + |1>")
+            qc.h(counter)
+            #expanded_values.append(qc)
+            # expanded_values.append("|0> + |1>")
         else:
-            expanded_values.append("|0> - |1>")
-    print(expanded_values)
+            qc.x(counter)
+            qc.h(counter)
+            #expanded_values.append(qc)
+            # expanded_values.append("|0> - |1>")
+        counter += 1
+    # for x in expanded_values:
+    #     backend = Aer.get_backend('statevector_simulator')
+    #     job = execute(x, backend=backend, shots=500, memory=True)
+    #     job_result = job.result()
+    #     print(job_result.get_statevector(x))
 
-#XOR qiskit time !
-            
+    #XOR qiskit time !
+    # To implement XOR, we can CNOT the second bit with the target bit, and then CNOT the third bit with the target bit
+    qc.cx(1,4)
+    qc.cx(2,4)
+    qc.measure(4,0)
     
-    
-    
-    
-    
-    
-    
+    aer_sim = Aer.get_backend('aer_simulator')
+    job = aer_sim.run(qc)
+    print(job.result().get_counts())
+    print(qc)
     
     
     
@@ -66,3 +83,34 @@ elif measure == "x" and (values[0] == "+" or values[0] == "-"):
     elif values[3] == "-" and tempThree:
         values[3] = "+"
     print("|" + str(values) +"> " + "|" + str(classical_bit) + ">")
+
+
+elif measure == "x" and (values[0] == "1" or values[0] == "0"):
+    # First we have create states of 0 and 1, then we apply hadamard to read in x basis
+    # 5 bits (4 for storing data and 1 for storing XOR vals)
+    qc = QuantumCircuit(5,1)
+    counter = 0
+    for val in values:
+        if val == "1":
+            qc.x(counter)
+            qc.h(counter)
+        else:
+            qc.h(counter)
+        counter += 1
+    # for x in expanded_values:
+    #     backend = Aer.get_backend('statevector_simulator')
+    #     job = execute(x, backend=backend, shots=500, memory=True)
+    #     job_result = job.result()
+    #     print(job_result.get_statevector(x))
+
+    #XOR qiskit time !
+    # To implement XOR, we can CNOT the second bit with the target bit, and then CNOT the third bit with the target bit
+    qc.cx(1,4)
+    qc.cx(2,4)
+    qc.measure(4,0)
+    
+    aer_sim = Aer.get_backend('aer_simulator')
+    job = aer_sim.run(qc)
+    print(job.result().get_counts())
+    print(qc)
+        
