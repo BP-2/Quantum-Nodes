4a39fcf692f85c7346cd4dcf430ed7a8b9203c00
Brady Phelps <bp309420@ohio.edu>
2023-07-25 11:26:58 -0400

    Simplified proof but abstract

--- Simulation/Simplified-Proof-Abstract.py
+++ Simulation/Simplified-Proof-Abstract.py
@@ -1,7 +1,5 @@
 # BP-2
-# Simulation of the simplified proof
-
-
+# Simulation of the simplified proof (abstract)
 from qiskit import * 
 from qiskit.tools.visualization import plot_histogram
 from IPython.display import display
@@ -24,44 +22,84 @@ while x < 4:
 if measure == "z" and (values[0] == "+" or values[0] == "-"):    
     # First we have to break '+' and '-' into |0> + |1> and |0> - |1> respectively
     
-    # 5 bits (4 for storing data and 1 for storing XOR vals)
-    qc = QuantumCircuit(5,1)
+    expanded_values = []
     counter = 0
     for val in values:
+        temp_val = []
         if val == "+":
-            qc.h(counter)
-            #expanded_values.append(qc)
-            # expanded_values.append("|0> + |1>")
+            temp_val.append(0)
+            temp_val.append(1)
+            expanded_values.append(temp_val)
         else:
-            qc.x(counter)
-            qc.h(counter)
-            #expanded_values.append(qc)
-            # expanded_values.append("|0> - |1>")
+            temp_val.append(0)
+            temp_val.append(-1)
+            expanded_values.append(temp_val)
         counter += 1
     # for x in expanded_values:
-    #     backend = Aer.get_backend('statevector_simulator')
-    #     job = execute(x, backend=backend, shots=500, memory=True)
-    #     job_result = job.result()
-    #     print(job_result.get_statevector(x))
+    #     print(x)
+        
+    # Now we only need to deal with the middle two terms
+    # We will tensor the two terms together
+    index = 1
+    inner_index = 0
+    tensor_values = []
 
-    #XOR qiskit time !
-    # To implement XOR, we can CNOT the second bit with the target bit, and then CNOT the third bit with the target bit
-    qc.cx(1,4)
-    qc.cx(2,4)
-    qc.measure(4,0)
+    temp_val = []
+    temp_val.append(expanded_values[index][inner_index])
+    temp_val.append(expanded_values[index+1][inner_index])
+    tensor_values.append(temp_val)
+    temp_val = []
+    temp_val.append(expanded_values[index][inner_index])
+    temp_val.append(expanded_values[index+1][inner_index + 1])
+    tensor_values.append(temp_val)
+    temp_val = []
+    temp_val.append(expanded_values[index][inner_index+1])
+    temp_val.append(expanded_values[index+1][inner_index])
+    tensor_values.append(temp_val)
+    temp_val = []
+    temp_val.append(expanded_values[index][inner_index+1])
+    temp_val.append(expanded_values[index+1][inner_index+1])
+    tensor_values.append(temp_val)
+        
+    zero_ends = []
+    one_ends = []
+    # Now we take the parity of the bits (ignoring sign)
+    for x in tensor_values:
+        parity = abs(x[0]) ^ abs(x[1])
+        x.append(parity)
+        if parity == 1:
+            one_ends.append(x)
+        else:
+            zero_ends.append(x)
+        
+    # for x in tensor_values:
+    #     print(x)
+        
+    # Now we condense like terms
+    condensed_vals = []
     
-    aer_sim = Aer.get_backend('aer_simulator')
-    job = aer_sim.run(qc)
-    print(job.result().get_counts())
-    print(qc)
+    term = "("
+    if ((zero_ends[0][0] < 0 or zero_ends[0][1] < 0) and zero_ends[0][0] * zero_ends[0][1] <= 0):
+        term += "(-)"
+    term += "|" + str(zero_ends[0][0]) + str(zero_ends[0][1])+"> +"
+    if ((zero_ends[1][0] < 0 or zero_ends[1][1] < 0) and zero_ends[1][0] * zero_ends[1][1] <= 0):
+        term += "(-)"
+    term += "|" + str(abs(zero_ends[1][0])) + str(abs(zero_ends[1][1]))+">)|0>"
+    condensed_vals.append(term)
     
-    
-    
+    term = "("
+    if ((one_ends[0][0] < 0 or one_ends[0][1] < 0) and one_ends[0][0] * one_ends[0][1] <= 0):
+        term += "(-)"
+    term += "|" + str(abs(one_ends[0][0])) + str(abs(one_ends[0][1]))+"> +"
+    if ((one_ends[1][0] < 0 or one_ends[1][1] < 0) and one_ends[1][0] * one_ends[1][1] <= 0):
+        term += "(-)"
+    term += "|" + str(abs(one_ends[1][0])) + str(abs(one_ends[1][1]))+">)|1>"
+    condensed_vals.append(term)
+        
+    for x in condensed_vals:
+        print(x)    
     # 00, - 01 + 10
-    
-    
-    
-    
+       
 elif measure == "z" and (values[0] == "1" or values[0] == "0"):
     classical_bit = int(values[1]) ^ int(values[2])
     values[3] = (int(values[2]) + int(values[1])) % 2
