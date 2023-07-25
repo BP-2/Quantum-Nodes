b9146a3db3944f8d2f1105e529b433706ab6c9e4
Brady Phelps <bp309420@ohio.edu>
2023-07-25 14:54:04 -0400

    All cases

--- Simulation/Simplified-Proof-Abstract.py
+++ Simulation/Simplified-Proof-Abstract.py
@@ -19,10 +19,19 @@ while x < 4:
 
 
 # Lets solve !!!
-if measure == "z" and (values[0] == "+" or values[0] == "-"):    
-    # First we have to break '+' and '-' into |0> + |1> and |0> - |1> respectively
+if (measure == "z" and (values[0] == "+" or values[0] == "-") or (measure == "x" and (values[0] == "1" or values[0] == "0"))):    
     
+    # First we have to break '+' and '-' into |0> + |1> and |0> - |1> respectively
     expanded_values = []
+    counter = 0
+    while counter < 4:
+        if values[counter] == "0":
+            values[counter] = "+"
+        elif values[counter] == "1":
+            values[counter] = "-"
+        counter += 1
+    for x in values:
+        print(x)
     counter = 0
     for val in values:
         temp_val = []
@@ -128,34 +137,3 @@ elif measure == "x" and (values[0] == "+" or values[0] == "-"):
     elif values[3] == "-" and tempThree:
         values[3] = "+"
     print("|" + str(values) +"> " + "|" + str(classical_bit) + ">")
-
-
-elif measure == "x" and (values[0] == "1" or values[0] == "0"):
-    # First we have create states of 0 and 1, then we apply hadamard to read in x basis
-    # 5 bits (4 for storing data and 1 for storing XOR vals)
-    qc = QuantumCircuit(5,1)
-    counter = 0
-    for val in values:
-        if val == "1":
-            qc.x(counter)
-            qc.h(counter)
-        else:
-            qc.h(counter)
-        counter += 1
-    # for x in expanded_values:
-    #     backend = Aer.get_backend('statevector_simulator')
-    #     job = execute(x, backend=backend, shots=500, memory=True)
-    #     job_result = job.result()
-    #     print(job_result.get_statevector(x))
-
-    #XOR qiskit time !
-    # To implement XOR, we can CNOT the second bit with the target bit, and then CNOT the third bit with the target bit
-    qc.cx(1,4)
-    qc.cx(2,4)
-    qc.measure(4,0)
-    
-    aer_sim = Aer.get_backend('aer_simulator')
-    job = aer_sim.run(qc)
-    print(job.result().get_counts())
-    print(qc)
-        
